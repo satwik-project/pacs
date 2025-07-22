104af6577ae431bb18e47c52d6c903be18ebdb49
beradesun <joy.desuntech@gmail.com>
2025-07-22 19:29:02 +0530

    all sone

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function ($) {
 
 
-
+	// curser
 	const cursor = document.querySelector(".cursor");
 	const follower = document.querySelector(".cursor-follower");
 	const cards = document.querySelectorAll(".timeline-item");
@@ -48,5 +48,50 @@ jQuery(document).ready(function ($) {
 		el.addEventListener("mouseleave", () => {
 			cursor.classList.remove("active");
 		});
+	});
+
+	// social slider
+
+	$('.js-address-slid').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 0,
+		speed: 8000,
+		arrows: false,
+		pauseOnHover: false,
+		cssEase: 'linear'
+	});
+
+	$('.js-team-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 0,
+		speed: 8000,
+		arrows: false,
+		pauseOnHover: false,
+		cssEase: 'linear'
+	});
+
+	// client
+	$('.js-client-txt-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		fade: true,
+		asNavFor: '.js-client-images-slider'
+	});
+	$('.js-client-images-slider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		asNavFor: '.js-client-txt-slider',
+		dots: false,
+		autoplay: true,
+		arrows: false,
+		centerMode: true,
+		focusOnSelect: true
 	});
 });
